f0ca69601170dcbe41c00003e81ce08d1664f9c9
David Larsson <manzey@hotmail.se>
2018-11-19 12:37:44 +0100

    Created schemas and test resolver (#1)

---
+++ backend/src/modules/resolvers/resolvers.js
@@ -0,0 +1,24 @@
+const roles = [
+  { id: 1, role: 'Admin' },
+  { id: 2, role: 'User' },
+  { id: 3, role: 'ParkingOwner' },
+];
+
+export const resolvers = {
+  Query: {
+    roleByName: (root, args, context, info) => {
+      return getRoleName(args.role)
+    },
+  },
+//  Mutation: {
+//  },
+};
+
+// Testing
+function getRoleName(name) {
+  for(let r of roles) {
+    if (r.role == name) {
+      return r
+    }
+  }
+}
