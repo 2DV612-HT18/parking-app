963f799a8c52ba262a576817bdbac23f368afeae
David Larsson <manzey@hotmail.se>
2018-11-20 13:32:35 +0100

    Add role if it doesn't exist

--- backend/src/modules/addRole/resolvers.js
+++ backend/src/modules/addRole/resolvers.js
@@ -5,8 +5,16 @@ export const resolvers = {
   Mutation: {
     addRole: async (_, args) => {
       let connection = getConnection()
-      const role = new Role(args.id, args.role)     
-      return connection.manager.save(role)
+      let role = new Role(args.id, args.role)
+      let roleRepository = connection.getRepository(Role)
+      let data = await roleRepository.find({ where: {role: args.role }})
+
+      if (data.length < 1) {        
+        return connection.manager.save(role)
+      }
+      else {
+        // Throw Error?
+      }
     },
   },
 };
