29fbfb8abf3274a0c5bdfac2aac9e5ffe34cba55
David Larsson <manzey@hotmail.se>
2018-11-21 15:50:09 +0100

    Hash password before storing in db

--- backend/src/modules/registerUser/resolvers.js
+++ backend/src/modules/registerUser/resolvers.js
@@ -1,24 +1,27 @@
 import User from "../../models/User";
 import {getConnection} from "typeorm";
-import { bcrypt } from "bcrypt";
+const bcrypt = require('bcrypt');
 
 export const resolvers = {
   Mutation: {
     registerUser: async (_, args) => {
       let connection = getConnection()
-      let user = new User(args.id, args.role, args.firstName, args.lastName, args.email, args.personalNumber, args.password)
-      let userRepository = connection.getRepository(User)
-      
-      // Query the database to check if user exists with email specified.
-      let data = await userRepository.find({ where: {email: args.email }})
-
-      // If user with specified email doesn't exist, save the user to the database.
-      if (data.length < 1) {        
-        return connection.manager.save(user)
-      }
-      else {
-        // Throw Error?
-      }
+      let saltRounds = 6
+      bcrypt.hash(args.password, saltRounds, async (err, hashedPassword) => {      
+        let user = new User(args.id, args.role, args.firstName, args.lastName, args.email, args.personalNumber, hashedPassword)
+        let userRepository = connection.getRepository(User)
+        
+        // Query the database to check if user exists with email specified.
+        let data = await userRepository.find({ where: {email: args.email }})
+        
+        // If user with specified email doesn't exist, save the user to the database.
+        if (data.length < 1) { 
+          return connection.manager.save(user)
+        }
+        else {
+          // Throw Error?
+        }
+      })
     },
   },
 };
