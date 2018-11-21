944993581f3f9c9f375335156e6309eed7c081e4
Marcus Gustafsson <marcusgu@hotmail.com>
2018-11-21 15:36:35 +0100

    reversed index.html

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import Role from "./models/Role";
 import createTypeormConnection from "./lib/createTypeormConnection";
 import generateSchema from "./lib/generateSchema";
 
+
 (async () => {
   try {
     const server = new GraphQLServer({
@@ -13,14 +14,17 @@ import generateSchema from "./lib/generateSchema";
       const role1 = new Role(21, "User")
       const role2 = new Role(22, "ParkingOwner")
       let data = await connection.getRepository(Role).find({ where: {role: role1.role }})
-      if (data.length < 1)    
+      if (data.length < 1)
       return connection.manager.save([role1, role2])
     });
-    
+
+
     server.start(() =>
       console.log("Server is running on http://localhost:4000/")
     );
   } catch (error) {
     console.error(error);
   }
+
+
 })();
