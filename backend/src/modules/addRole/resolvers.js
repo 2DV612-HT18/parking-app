e6762adc435dee7058ab60ada28fc8d30a1b2fe4
David Larsson <manzey@hotmail.se>
2018-11-21 15:38:46 +0100

    Add bcrypt and comments

--- backend/src/modules/addRole/resolvers.js
+++ backend/src/modules/addRole/resolvers.js
@@ -7,8 +7,11 @@ export const resolvers = {
       let connection = getConnection()
       let role = new Role(args.id, args.role)
       let roleRepository = connection.getRepository(Role)
+
+      // Query the database to check if role exists with rolename specified.
       let data = await roleRepository.find({ where: {role: args.role }})
 
+      // If no role of the same name exists, add it. 
       if (data.length < 1) {        
         return connection.manager.save(role)
       }
--- backend/src/modules/registerUser/resolvers.js
+++ backend/src/modules/registerUser/resolvers.js
@@ -1,5 +1,6 @@
 import User from "../../models/User";
 import {getConnection} from "typeorm";
+import { bcrypt } from "bcrypt";
 
 export const resolvers = {
   Mutation: {
@@ -7,6 +8,8 @@ export const resolvers = {
       let connection = getConnection()
       let user = new User(args.id, args.role, args.firstName, args.lastName, args.email, args.personalNumber, args.password)
       let userRepository = connection.getRepository(User)
+      
+      // Query the database to check if user exists with email specified.
       let data = await userRepository.find({ where: {email: args.email }})
 
       // If user with specified email doesn't exist, save the user to the database.
