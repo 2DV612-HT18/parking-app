1c1af60e72f1490ecac7031131fa1d0527322d44
Emy <emyconstella@gmail.com>
2018-11-22 19:23:47 +0100

    Refactored index.js with async + await

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,21 +3,23 @@ import Role from "./models/Role";
 import createTypeormConnection from "./lib/createTypeormConnection";
 import generateSchema from "./lib/generateSchema";
 
-
 (async () => {
   try {
     const server = new GraphQLServer({
       schema: generateSchema()
     });
 
-    await createTypeormConnection("development").then(async (connection) => {
-      const role1 = new Role(21, "User")
-      const role2 = new Role(22, "ParkingOwner")
-      let data = await connection.getRepository(Role).find({ where: {role: role1.role }})
-      if (data.length < 1)
-      return connection.manager.save([role1, role2])
-    });
+    const connection = await createTypeormConnection("development");
 
+    // Save standard roles if role table is empty
+    const role1 = new Role(21, "User");
+    const role2 = new Role(22, "ParkingOwner");
+    const data = await connection
+      .getRepository(Role)
+      .find({ where: { role: role1.role } });
+    if (data.length < 1) {
+      await connection.manager.save([role1, role2]);
+    }
 
     server.start(() =>
       console.log("Server is running on http://localhost:4000/")
@@ -25,6 +27,4 @@ import generateSchema from "./lib/generateSchema";
   } catch (error) {
     console.error(error);
   }
-
-
 })();
