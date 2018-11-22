9cf6cb9aad47aaf49c3f6485f4503de96e19d045
Emy <emyconstella@gmail.com>
2018-11-22 21:42:54 +0100

    Lint fixes

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -8,49 +8,42 @@
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() 
-        {
-            return {
-                input: 
-                {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: 
-        {
-            login() 
-            {
-                if(this.input.username != "" && this.input.password != "") 
-                {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) 
-                    {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } 
-                    else 
-                    {
-                        alert("The username and / or password is incorrect")
-                    }
-                } 
-                else 
-                {
-                    alert("A username and password must be present");
-                }
-            }
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      if (this.input.username != "" && this.input.password != "") {
+        if (
+          this.input.username == this.$parent.mockAccount.username &&
+          this.input.password == this.$parent.mockAccount.password
+        ) {
+          this.$emit("authenticated", true);
+          this.$router.replace({ name: "secure" });
+        } else {
+          alert("The username and / or password is incorrect");
         }
+      } else {
+        alert("A username and password must be present");
+      }
     }
+  }
+};
 </script>
 <style scoped>
-    #login {
-        width: 500px;
-        border: 1px solid #CCCCCC;
-        background-color: #FFFFFF;
-        margin: auto;
-        margin-top: 200px;
-        padding: 20px;
-    }
+#login {
+  width: 500px;
+  border: 1px solid #cccccc;
+  background-color: #ffffff;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
 </style>
