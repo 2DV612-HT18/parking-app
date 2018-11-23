d46b7b2ddb0b5e46bd64c3c4da7293d79343af12
Emy <emyconstella@gmail.com>
2018-11-23 02:17:44 +0100

    Refactoring and lint fixes

--- backend/src/modules/registerUser/resolvers.js
+++ backend/src/modules/registerUser/resolvers.js
@@ -1,39 +1,41 @@
-import {getConnection} from "typeorm";
+import { getConnection } from "typeorm";
 import User from "../../models/User";
 import emailSender from "../../nodemailer";
 
 const bcrypt = require("bcrypt");
 
-
 export const resolvers = {
   Mutation: {
     registerUser: async (_, args) => {
-      let connection = getConnection()
-      let saltRounds = 6
+      const connection = getConnection();
+      const saltRounds = 6;
 
-      // await Promise for the password to be hashed before proceeding
-      const hashedPassword = await new Promise((resolve, reject) => {
-        bcrypt.hash(args.password, saltRounds, async (err, hash) => {
-          if (err)
-          reject(err)
-          resolve(hash)
-        })
-      })
-      
-      let user = new User(args.role, args.firstName, args.lastName, args.email, args.personalNumber, hashedPassword)
-      let userRepository = connection.getRepository(User)
+      const hashedPassword = await bcrypt.hash(args.password, saltRounds);
+
+      const user = new User(
+        0,
+        args.role,
+        args.firstName,
+        args.lastName,
+        args.email,
+        args.personalNumber,
+        hashedPassword
+      );
+      const userRepository = connection.getRepository(User);
 
       // Query the database to check if user exists with email specified.
-      let data = await userRepository.find({ where: {email: args.email }})
+      const data = await userRepository.find({ where: { email: args.email } });
 
       // If user with specified email doesn't exist, save the user to the database.
       if (data.length < 1) {
         // call send email function
-        emailSender.sendEmail(args.email);        
-        return connection.manager.save(user) 
-      } else {
-        // Throw Error?
+        emailSender.sendEmail(args.email);
+        await connection.manager.save(user);
+
+        return user;
       }
-    },
-  },
+      // Throw Error?
+      return null;
+    }
+  }
 };
