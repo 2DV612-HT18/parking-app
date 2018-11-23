6fbe6089cd7aba610b017b24fd0a4295299357bb
Emy <emyconstella@gmail.com>
2018-11-23 02:06:38 +0100

    Added myInfo query

---
+++ backend/src/modules/myInfo/resolvers.js
@@ -0,0 +1,15 @@
+import { getConnection } from "typeorm";
+
+import User from "../../models/User";
+
+export const resolvers = {
+  Query: {
+    myInfo: async (_, __, { user }) => {
+      const connection = getConnection();
+      const userRepository = connection.getRepository(User);
+
+      // Return authenticated user
+      return userRepository.findOne({ where: { email: user.email } });
+    }
+  }
+};
--- backend/src/permissions.js
+++ backend/src/permissions.js
@@ -2,6 +2,9 @@ import { shield, not } from "graphql-shield";
 import isAuthenticated from "./lib/middlewares/isAuthenticated";
 
 export default shield({
+  Query: {
+    myInfo: isAuthenticated
+  },
   Mutation: {
     registerUser: not(isAuthenticated),
     loginUser: not(isAuthenticated)
