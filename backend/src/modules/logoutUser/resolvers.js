7681e58b484922909d31ac18ebe0dfcf84cf7902
David Larsson <manzey@hotmail.se>
2018-11-26 15:21:15 +0100

    Logout-mutation

---
+++ backend/src/modules/logoutUser/resolvers.js
@@ -0,0 +1,18 @@
+import { getConnection } from "typeorm";
+import jwt from "jsonwebtoken";
+import User from "../../models/User";
+
+export const resolvers = {
+  Mutation: {
+    logoutUser: async (_, args) => {
+      const connection = getConnection();
+      const userRepository = connection.getRepository(User);
+      const data = await userRepository.find({ where: { email: args.email } });
+
+      if (data.length === 1) {
+        const user = data[0];
+      }
+      return null;
+    }
+  }
+};
