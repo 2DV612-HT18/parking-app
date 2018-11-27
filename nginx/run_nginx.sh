a94a917a7ae2645461974ee93f6570fc7e378ccf
tobiasbernting <tobiasbernting@gmail.com>
2018-11-27 14:40:17 +0100

    separated front and backend into two conf files

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -2,4 +2,6 @@
 
 export DOLLAR='$'
 envsubst < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst < /etc/nginx/sites-enabled/backend.template > /etc/nginx/sites-enabled/backend.conf
+envsubst < /etc/nginx/sites-enabled/frontend.template > /etc/nginx/sites-enabled/frontend.conf
 nginx -g "daemon off;"
