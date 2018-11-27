b81832b2b74a7ae2e0cea0593ad34d981ead3071
Lars Pekkala <larrxi@gmail.com>
2018-11-27 21:22:46 +0700

    run verify on component mounted

--- frontend/src/components/VerifyEmail.vue
+++ frontend/src/components/VerifyEmail.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-<v-btn 
-                  color="primary" 
-                  v-on:click="verifyEmail();"
-                  >
-                  Submit
-                </v-btn>
         </div>
 </template>
 <script>
@@ -14,70 +8,40 @@ import { mapMutations } from "vuex";
 import VerifyEmail from "@/graphql/VerifyEmail.gql";
 
   export default {
-    /*
-    created() {
-    console.log(this.$route);
-    console.log(this.$router.currentRoute.query.token);
-    console.log("asd");
 
-      const result = await this.$apollo.mutate({
-        mutation: VerifyEmail,
-        variables: {
-          token: this.$router.currentRoute.query.token
-        }
-
-            });
-*/
-            
-     data: () => ({
+      data: () => ({
      
     }),
-    methods: {
-      async verifyEmail(){
-        console.log("Lets verify: " + this.$router.currentRoute.query.token);
+      methods: {
+        async verifyEmail(){
+          console.log("Lets verify with " + this.$router.currentRoute.query.token);
 
-     const result = await this.$apollo.mutate({
-        mutation: VerifyEmail,
-        variables: {
-          token: this.$router.currentRoute.query.token
-        }
+          const result = await this.$apollo.mutate({
+            mutation: VerifyEmail,
+              variables: {
+                token: this.$router.currentRoute.query.token
+              }
 
-            });
+          });
 
-      const data = result.data.verifyEmail;
-      // Token exists
-      if (data ) {
-        
-          console.log(data);
-        // Redirect to homepage
-       // this.$router.push("/");
-        console.log("email verify successfull");
-      } else {
-        // register unsuccessful
-        console.log("email verify unsuccessfull");
-         console.log(data);
-      }
+          const data = result.data.verifyEmail;
+          if (data ) {
+            console.log("email verify successfull");
+          } else {
+            console.log("email verify unsuccessfull");
+          }
 
-      }
-    
-  //  this.baseaccount = this.$route.params.baseaccount;
-  }
- // const data = result.data.registerUser;
-      // Token exists
-     // if (data ) {
+        } 
         
 
-        // Redirect to homepage
-      //  this.$router.push("/");
-       // console.log("successfull: " + this.email);
-      //} else {
-        // register unsuccessful
-       // console.log("unsuccessfull: " + this.email);
-     // }
+      },
 
-    
-
+      created () {
+        this.verifyEmail();
+          
       }
+
+    }
 
 
 </script>
