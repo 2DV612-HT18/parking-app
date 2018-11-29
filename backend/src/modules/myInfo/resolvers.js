b19cc9986d9a3311447845e73f125885a5b2b534
Emy <emyconstella@gmail.com>
2018-11-29 16:33:17 +0100

    Fetching vehicles from vehicle repository

--- backend/src/modules/myInfo/resolvers.js
+++ backend/src/modules/myInfo/resolvers.js
@@ -1,14 +1,28 @@
 import { getConnection } from "typeorm";
 
 import User from "../../models/User";
+import Vehicle from "../../models/Vehicle";
 
 export const resolvers = {
   Query: {
     myInfo: async (_, __, { user }) => {
       const connection = getConnection();
       const userRepository = connection.getRepository(User);
+      const vehicleRepository = connection.getRepository(Vehicle);
 
-      return userRepository.findOne({ relations: ["vehicles"],where: { id: user.id } });
+      const myUser = await userRepository.findOne({
+        where: { id: user.id }
+      });
+
+      // Get user vehicles
+      const vehicles = await vehicleRepository.find({
+        where: {
+          user: myUser
+        }
+      });
+
+      // Merge vehicles into myUser object
+      return { ...myUser, vehicles };
     }
   }
 };
