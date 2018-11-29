a260071fdfc6e6f5b69f2681da7afe5cdfacc4fb
Lars Pekkala <larrxi@gmail.com>
2018-11-29 19:55:58 +0700

    Add vehicles to myInfo

--- backend/src/modules/myInfo/resolvers.js
+++ backend/src/modules/myInfo/resolvers.js
@@ -1,6 +1,7 @@
 import { getConnection } from "typeorm";
 
 import User from "../../models/User";
+import Vehicle from "../../models/Vehicle";
 
 export const resolvers = {
   Query: {
@@ -10,6 +11,13 @@ export const resolvers = {
 
       // Return authenticated user
       return userRepository.findOne({ where: { id: user.id } });
+    },
+    vehicles: async (_, __, { user }) => {
+      const connection = getConnection();
+      const vehicleRepository = connection.getRepository(Vehicle);
+
+      // Return vehivles
+      return vehicleRepository.find({ where: { user: user} });
     }
   }
 };
