8e0b4560e6c353768f49a35c4a46656d8f569007
Lars Pekkala <larrxi@gmail.com>
2018-11-29 10:31:00 +0700

    Added v-alert to show verification result

--- frontend/src/components/VerifyEmail.vue
+++ frontend/src/components/VerifyEmail.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-        </div>
+<div>
+  <v-alert
+      type="success"
+      :value="success"
+    >
+    Your email address has been successfully verified.
+    </v-alert>
+
+ <v-alert
+      :value="fail"
+      type="error"
+    >
+      Your email address failed to verify.
+    </v-alert>
+
+    </div>
 </template>
+
 <script>
 
 import { mapMutations } from "vuex";
@@ -10,7 +25,8 @@ import VerifyEmail from "@/graphql/VerifyEmail.gql";
   export default {
 
       data: () => ({
-     
+     success: false,
+     fail: false
     }),
       methods: {
         async verifyEmail(){
@@ -27,8 +43,10 @@ import VerifyEmail from "@/graphql/VerifyEmail.gql";
           const data = result.data.verifyEmail;
           if (data ) {
             console.log("email verify successfull");
+            this.success=true;
           } else {
             console.log("email verify unsuccessfull");
+            this.fail=true
           }
 
         } 
