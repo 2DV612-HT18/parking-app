8ac8bbb427dd9fd1a66d1bc6367a355102c07fc7
Lars Pekkala <larrxi@gmail.com>
2018-11-29 21:06:20 +0700

    hämta bilarna samtidigt

--- backend/src/modules/myInfo/resolvers.js
+++ backend/src/modules/myInfo/resolvers.js
@@ -1,7 +1,6 @@
 import { getConnection } from "typeorm";
 
 import User from "../../models/User";
-import Vehicle from "../../models/Vehicle";
 
 export const resolvers = {
   Query: {
@@ -9,16 +8,7 @@ export const resolvers = {
       const connection = getConnection();
       const userRepository = connection.getRepository(User);
 
-      const oneUser = await userRepository.findOne({ where: { id: user.id } });
-
-      const vehicleRepository = connection.getRepository(Vehicle);
-
-      oneUser.vehicles = await vehicleRepository.find({ where: { user: oneUser} });
-
-      // Return authenticated user
-      return oneUser
-
-
+      return await userRepository.findOne({ relations: ["vehicles"],where: { id: user.id } });
     }
   }
 };
