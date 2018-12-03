3a96dc08d725f9546bd3ca9099fe6e969d004b7b
David Larsson <manzey@hotmail.se>
2018-12-03 20:46:52 +0100

    Snackbar!

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,6 +1,8 @@
 <template>
   <div><Register 
     :form_title=form_title
+    :success_message=success_message
+    :failed_message=failed_message
     :mutationName=mutationName
   /></div>
 </template>
@@ -15,6 +17,8 @@ export default {
   data() {
     return {
       form_title: "Register",
+      success_message: "You successfully registered your account!",
+      failed_message: "Email is already taken!",
       mutationName: "RegisterUser"
     };
   }
