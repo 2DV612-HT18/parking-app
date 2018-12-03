8cc968df293363f6cd16b30a6fa3254708b927d7
David Larsson <manzey@hotmail.se>
2018-12-03 15:51:31 +0100

    Role Model admin

--- backend/src/models/Role.js
+++ backend/src/models/Role.js
@@ -1,7 +1,8 @@
 export default class Role {
-  constructor(id, name, registration) {
+  constructor(id, name, registration, admin) {
     this.id = id;
     this.name = name;
     this.registration = registration;
+    this.admin = admin;
   }
 }
