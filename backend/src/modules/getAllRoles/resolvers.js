25fc08972e8ff0c35f1327d36d8982f20835b5ce
Emy <emyconstella@gmail.com>
2018-12-07 06:46:57 +0100

    Fixed roles not found when user is not in database

--- backend/src/modules/getAllRoles/resolvers.js
+++ backend/src/modules/getAllRoles/resolvers.js
@@ -1,27 +1,29 @@
+import { getConnection } from "typeorm";
 import Role from "../../models/Role";
 import User from "../../models/User";
-import {getConnection} from "typeorm";
 
 export const resolvers = {
   Query: {
     getAllRoles: async (_, args, { user }) => {
-      let data = ""
-      const connection = getConnection()
-      const roleRepository = connection.getRepository(Role)      
+      let data = "";
+      const connection = getConnection();
+      const roleRepository = connection.getRepository(Role);
       const userRepository = connection.getRepository(User);
 
       // If user is logged in, get user from database, else currentUser is false, which means user is not logged in.
-      const currentUser = user ? await userRepository.findOne({
-        where: { id: user.id },
-        relations: ["roles"]
-      }) : false;
+      const currentUser = user
+        ? await userRepository.findOne({
+            where: { id: user.id },
+            relations: ["roles"]
+          })
+        : false;
 
-      if (user && currentUser.roles[0].admin) {
-        data = await roleRepository.find()
+      if (currentUser && currentUser.roles[0].admin) {
+        data = await roleRepository.find();
       } else {
-        data = await roleRepository.find({ where: { registration: true } })
-      }     
-      return data
-    },
-  },
+        data = await roleRepository.find({ where: { registration: true } });
+      }
+      return data;
+    }
+  }
 };
