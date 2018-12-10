e11e66de9a31906d9a8979d5c6c89bb56f7643f3
baangfilip <baangfilip@gmail.com>
2018-12-10 16:58:05 +0100

    Parking area views

---
+++ frontend/src/views/area/View.vue
@@ -0,0 +1,13 @@
+<template>
+  <View-parking-area/>
+</template>
+
+<script>
+import ViewParkingArea from "@/components/ViewParkingArea.vue";
+
+export default {
+  components: {
+    ViewParkingArea
+  }
+}
+</script>
