7343c55304e028d22bb28ff23ab0e33d880ac1a0
David Larsson <manzey@hotmail.se>
2018-12-10 19:36:15 +0100

    dismissNotifications

---
+++ backend/src/modules/dismissNotification/resolvers.js
@@ -0,0 +1,30 @@
+import { getConnection } from "typeorm";
+import User from "../../models/User";
+import Notification from "../../models/Notification"
+
+export const resolvers = {
+  Mutation: {
+    dismissNotification: async (_, args, { user }) => {
+      const connection = getConnection();
+      const userRepository = connection.getRepository(User);
+      const notificationRepository = connection.getRepository(Notification); 
+      const loggedInUser = await userRepository.findOne({
+        where: { id: user.id }
+      });
+
+      const notification = await notificationRepository.findOne({
+        where: {id: args.id},
+        relations: ["author", "dismissed"] 
+      });      
+
+      // If successfully found logged in user and notification, push the logged in user to the dismissed array.
+      if (loggedInUser && notification) {
+        notification.dismissed.push(loggedInUser);
+        await connection.manager.save(notification);
+        return true;
+      } else {
+        return false;
+      }      
+    }
+  }
+};
