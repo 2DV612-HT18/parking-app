b666c027165263dd77dc5880424aa0bface665ec
Inanimis <0.inanimis@gmail.com>
2018-12-11 14:04:21 +0100

    Create notification

---
+++ frontend/src/components/Notification.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>         
+      <v-card class="elevation-12">              
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>{{ form_title }}</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form v-model="validForm">
+            <v-text-field
+              v-model="notification"
+              label="Notification"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn to="/">Cancel</v-btn>
+          <v-btn color="primary" v-on:click="sendMsg();">Send</v-btn>
+        </v-card-actions>
+      </v-card>       
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  props: ['form_title', 'mutationName', 'success_message'],
+  data: () => ({
+    notification: "",
+    success: false,
+  }),    
+  methods: {
+    async sendMsg() {
+      const result = await this.$apollo.mutate({
+        mutation: eval(this.mutationName),
+        variables: {
+          notification: this.notification,
+        }
+      });
+    }
+  }  
+}
+</script>
