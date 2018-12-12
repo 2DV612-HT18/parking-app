fdbf1276bb08284adf9dac2fc1781767676304f9
Emy <emyconstella@gmail.com>
2018-12-12 03:19:20 +0100

    Created REPEAT enum class

---
+++ backend/src/lib/rates/REPEAT.js
@@ -0,0 +1,21 @@
+export default class REPEAT {
+  static get NEVER() {
+    return "NEVER";
+  }
+
+  static get DAILY() {
+    return "DAILY";
+  }
+
+  static get WEEKLY() {
+    return "WEEKLY";
+  }
+
+  static get MONTHLY() {
+    return "MONTHLY";
+  }
+
+  static get YEARLY() {
+    return "YEARLY";
+  }
+}
