4b45bedf230ea32c86b7ab2d14291491c9bd9f3b
Ulf TObias Bernting <tobiasbernting@gmail.com>
2018-12-12 11:26:15 +0100

    copies the cert files to nginx container and extracts them

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#Extract the cert files.
+cd /etc/nginx/certs && tar xvf /etc/nginx/backup.tar --strip 1
+
 export DOLLAR='$'
 envsubst < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 envsubst < /etc/nginx/sites-enabled/backend.template > /etc/nginx/sites-enabled/backend.conf
